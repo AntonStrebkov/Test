8a5cf5cafeca5264b99927d7725a776b35195e62
AntonStrebkov <wancler987@gmail.com>
2022-08-03 23:49:23 +0700

    add Ex005 Love

---
+++ Ex005_Love/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ксюша")
+{
+    Console.WriteLine("УРА! Это же Ксюша!");
+}
+
+else
+{
+    Console.WriteLine("Здравствуйте, ");
+    Console.WriteLine(username);
+}
