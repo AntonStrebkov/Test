bad8d19558fa525ccf63ae25eb333bb8dc9227ff
AntonStrebkov <wancler987@gmail.com>
2022-08-04 20:49:53 +0700

    add Ex008 Sport

---
+++ Ex008_Sport/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 3; 
+int b = 5; 
+int c = 8; 
+int d = 2; 
+int e = 4; 
+int max = a; 
+if(b > max) 
+{ 
+    max = b; 
+} 
+if(c > max) 
+{ 
+    max = c; 
+} 
+if(d > max) 
+{ 
+    max = d; 
+} 
+if(e > max) 
+{ 
+    max = e; 
+} 
+Console.WriteLine("Наибольшее значение = " + max);
